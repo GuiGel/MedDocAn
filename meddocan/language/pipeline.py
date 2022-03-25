98079047e2d1f0e2aa9e707f2b3748d593b5e4df
ggelabert <ggelabert@serikat.es>
2022-03-25 10:02:02 +0100

    :construction: Update eorganise the tokenisation

--- meddocan/language/pipeline.py
+++ meddocan/language/pipeline.py
@@ -18,9 +18,46 @@ def meddocan_pipeline() -> MeddocanLanguage:
     """
     nlp = spacy.blank("es")
     nlp.tokenizer = meddocan_tokenizer(nlp)
-    # nlp.add_pipe("sentencizer")
     nlp.add_pipe(
         "missaligned_splitter",
         config={"words": ["NºCol", "Correo", "años", "DR", "\.$"]},
     )
     return MeddocanLanguage(nlp)
+
+
+if __name__ == "__main__":
+
+    nlp = meddocan_pipeline()
+
+    to_tokenize = [
+        ("H.", ["H", "."]),
+        ("M.", ["M", "."]),
+        ("F.", ["F", "."]),
+        ("nhc-150679.", ["nhc", "-", "150679", "."]),
+        ("CP:28029", ["CP", ":", "28029"]),
+        ("2014.El", ["2014", ".", "El"]),
+        ("11A.", ["11A", "."]),
+        # Occurs a lots in the text. Perhaps making a special rules make sense?
+        ("s/n.", ["s/n", "."]),
+        ("Der..", ["Der", ".", "."]),
+        ("Izq..", ["Izq", ".", "."]),
+        ("der..", ["der", ".", "."]),
+        ("izq..", ["izq", ".", "."]),
+        ("1964)10", ["1964", ")", "10"]),
+        ("+34679802102", ["+", "34679802102"]),
+        (
+            "E-mail:guacorch@yahoo.es",
+            ["E", "-", "mail", ":", "guacorch@yahoo", ".", "es"],
+        ),
+        (
+            "E-mail.hleonbrito@hotmail.com",
+            ["E", "-", "mail", ".", "hleonbrito@hotmail", ".", "com"],
+        ),
+        ("nhc/976421.", ["nhc", "/", "976421", "."]),
+    ]
+
+    for text, tokens in to_tokenize:
+        if [token.orth_ for token in nlp(text)] != tokens:
+            print("-" * 100)
+            for t in nlp.tokenizer.explain(text):
+                print(t[1], "\t", t[0])
--- tests/language/test_tokenizer.py
+++ tests/language/test_tokenizer.py
@@ -1,26 +1,23 @@
 import pytest
+import spacy
 
-from meddocan.language.pipeline import meddocan_pipeline
+from meddocan.language.tokenizer import meddocan_tokenizer
 
 
 @pytest.mark.parametrize(
-    "text,expected",
-    [
-        ("NºCol FerrándezCorreo", ["NºCol", "Ferrández", "Correo"]),
-        ("NºColMartínez", ["NºCol", "Martínez"]),
+    argnames="text,tokens",
+    argvalues=[
         ("H.", ["H", "."]),
         ("M.", ["M", "."]),
         ("F.", ["F", "."]),
+        ("Der..", ["Der", ".."]),
+        ("Izq..", ["Izq", ".."]),
+        ("der..", ["der", ".."]),
+        ("izq..", ["izq", ".."]),
         ("nhc-150679.", ["nhc", "-", "150679", "."]),
         ("CP:28029", ["CP", ":", "28029"]),
         ("2014.El", ["2014", ".", "El"]),
         ("11A.", ["11A", "."]),
-        ("s/n.", ["s/n", "."]),
-        ("Der..", ["Der", ".", "."]),
-        ("Izq..", ["Izq", ".", "."]),
-        ("der..", ["der", ".", "."]),
-        ("izq..", ["izq", ".", "."]),
-        ("c.", ["c", "."]),
         ("1964)10", ["1964", ")", "10"]),
         ("+34679802102", ["+", "34679802102"]),
         (
@@ -34,9 +31,10 @@ from meddocan.language.pipeline import meddocan_pipeline
         ("nhc/976421.", ["nhc", "/", "976421", "."]),
     ],
 )
-def test_meddocan_pipeline(text, expected):
-    """Test that the ``meddocan_pipeline`` tokenize the sentence as expected."""
-    nlp = meddocan_pipeline()
-    doc = nlp(text)
-    for token, expected_text in zip(doc, expected):
-        assert token.text == expected_text
+def test_meddocan_tokenizer(text, tokens):
+    """Test that the tokenizer works as expected, i.e that the given "text"
+    is tokenize into the given "tokens".
+    """
+    nlp = spacy.blank("es")
+    nlp.tokenizer = meddocan_tokenizer(nlp)
+    assert [token.orth_ for token in nlp(text)] == tokens
