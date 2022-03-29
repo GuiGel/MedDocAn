2788cd86899c3a160fe00cb9a75f82635207d275
ggelabert <ggelabert@serikat.es>
2022-03-29 10:50:16 +0200

    :sparkles: Add language sentencizer

---
+++ meddocan/language/sentencizer.py
@@ -0,0 +1,28 @@
+"""This module implements a custom spacy component that splits a document into
+sentences.   
+Sentences are split when the sentencizer encounters a new line marker "\n".
+In this way, when we load a meddocan file ``.txt.``, we can load the file as
+``spacy.tokens.Document`` and split the document into lines easily in this way
+in this way:  
+
+```python
+for sent in doc.sents:
+    ... # Do some stuff on the sentence
+```
+
+Ref: https://spacy.io/usage/processing-pipelines#custom-components
+"""
+from spacy.language import Language
+
+
+@Language.component("line_sentencizer")
+def line_sentencizer(doc):
+    for i, token in enumerate(doc[:-2]):
+        # Define sentence start if pipe + titlecase token
+        if "\n" in token.text:
+            doc[i + 1].is_sent_start = True
+        else:
+            # Explicitly set sentence start to False otherwise, to tell
+            # the parser to leave those tokens alone
+            doc[i + 1].is_sent_start = False
+    return doc
