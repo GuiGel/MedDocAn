e9fa41e890c5789e31a2714e2c270e1522d2a060
ggelabert <ggelabert@serikat.es>
2022-04-05 16:46:31 +0200

    :sparkles: Add new write_methods component

--- meddocan/language/pipeline.py
+++ meddocan/language/pipeline.py
@@ -2,9 +2,11 @@ from typing import NewType, Optional
 
 from spacy import blank
 from spacy.language import Language
+from spacy.tokens import Doc
 
-# TODO Set tokenizer as a language to use entry points
-from .tokenizer import meddocan_tokenizer
+# TODO Define the tokenizer as a language for using entry points and give it a
+# real type at the same time.
+from meddocan.language.tokenizer import meddocan_tokenizer
 
 MeddocanLanguage = NewType("MeddocanLanguage", Language)
 
@@ -14,11 +16,33 @@ def meddocan_pipeline(
 ) -> MeddocanLanguage:
     """Create meddocan language.
 
+    Example:
+
+    >>> nlp = meddocan_pipeline()
+    >>> doc = nlp("Vivo en Bilbao.")
+    >>> doc
+    Vivo en Bilbao.
+
+    The document has an immutable extension ``is_meddocan_doc`` that permit to
+    know that the document has been produced by the ``meddocan_pipeline``.
+
+    >>> doc._.is_meddocan_doc
+    True
+
+    Verify for fun that the extension is immutable.
+    >>> doc._.is_meddocan_doc = False
+    >>> doc._.is_meddocan_doc
+    True
+
     Returns:
         MeddocanLanguage: A ``spacy.language.Language`` that preprocess the
             meddocan text in order to have alignment with the entities offsets.
     """
     nlp = blank("es")
+
+    # Add an immutable custom extension by using the getter argument.
+    Doc.set_extension("is_meddocan_doc", getter=lambda doc: True, force=True)
+
     nlp.tokenizer = meddocan_tokenizer(nlp)
     nlp.add_pipe(
         "missaligned_splitter",
@@ -29,42 +53,5 @@ def meddocan_pipeline(
         "predictor",
         config={"model_loc": model_loc, "mini_batch_size": mini_batch_size},
     )
+    nlp.add_pipe("write_methods")
     return MeddocanLanguage(nlp)
-
-
-if __name__ == "__main__":
-
-    nlp = meddocan_pipeline()
-
-    to_tokenize = [
-        ("H.", ["H", "."]),
-        ("M.", ["M", "."]),
-        ("F.", ["F", "."]),
-        ("nhc-150679.", ["nhc", "-", "150679", "."]),
-        ("CP:28029", ["CP", ":", "28029"]),
-        ("2014.El", ["2014", ".", "El"]),
-        ("11A.", ["11A", "."]),
-        # Occurs a lots in the text. Perhaps making a special rules make sense?
-        ("s/n.", ["s/n", "."]),
-        ("Der..", ["Der", ".", "."]),
-        ("Izq..", ["Izq", ".", "."]),
-        ("der..", ["der", ".", "."]),
-        ("izq..", ["izq", ".", "."]),
-        ("1964)10", ["1964", ")", "10"]),
-        ("+34679802102", ["+", "34679802102"]),
-        (
-            "E-mail:guacorch@yahoo.es",
-            ["E", "-", "mail", ":", "guacorch@yahoo", ".", "es"],
-        ),
-        (
-            "E-mail.hleonbrito@hotmail.com",
-            ["E", "-", "mail", ".", "hleonbrito@hotmail", ".", "com"],
-        ),
-        ("nhc/976421.", ["nhc", "/", "976421", "."]),
-    ]
-
-    for text, tokens in to_tokenize:
-        if [token.orth_ for token in nlp(text)] != tokens:
-            print("-" * 100)
-            for t in nlp.tokenizer.explain(text):
-                print(t[1], "\t", t[0])
