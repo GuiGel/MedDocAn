9eac8d38f947e5f421be4d1fe151ba2aaad6bd9b
ggelabert <ggelabert@serikat.es>
2022-04-07 12:53:05 +0200

    :wrench: Fix make linting an formating compatible

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,5 +5,5 @@ set -x
 
 export MYPYPATH=./stubs
 mypy --show-error-code meddocan
-black meddocan tests --check
-isort meddocan tests --check-only
+black -l 79 meddocan tests --check
+isort -l 79 meddocan tests --check-only
