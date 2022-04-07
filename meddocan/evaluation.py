9ea30e3658385f979c84fd778945f94a1b3859c2
ggelabert <ggelabert@serikat.es>
2022-04-07 11:15:56 +0200

    :hammer: Add data creation for evaluarion script

---
+++ meddocan/evaluation.py
@@ -0,0 +1,70 @@
+"""Module where the evaluation data are made for a given
+``flair.models.SequenceTagger`` object.
+
+The evaluation process can then be made using the command line available in the
+converter script ``library``_.
+
+.. libray:_``https://github.com/PlanTL-GOB-ES/MEDDOCAN-Format-Converter-Script``
+"""
+from pathlib import Path
+from typing import Union
+
+from meddocan.data import ArchiveFolder
+from meddocan.data.docs_iterators import GsDocs, SysDocs
+
+
+def produce_evaluation_data(
+    model: str,
+    name: str,
+    evaluation_root: Union[str, Path],
+    force: bool = False,
+) -> None:
+    """Create the files necessary for the ``evaluation.py'' script to produce
+    the results allowing MEDDOCAN to make a comparison of the results obtained
+    by the different participants.
+
+    The function produce the following folder hierarchy:
+
+    - evaluation_root
+    - evaluation_root.golds.test.brat
+    - evaluation_root.golds.test.brat.file-1.ann
+    - evaluation_root.golds.test.brat.file-1.txt
+    - ...
+    - evaluation_root.golds.test.brat.file-n.ann
+    - evaluation_root.golds.test.brat.file-n.ann
+    - evaluation_root.name.test.brat
+    - evaluation_root.name.test.brat.file-1.ann
+    - evaluation_root.name.test.brat.file-1.txt
+    - ...
+    - evaluation_root.name.test.brat.file-n.ann
+    - evaluation_root.name.test.brat.file-n.ann
+
+    Example:
+
+    TODO How to write doctest code that is fast enough with the necessity of
+    loading a model?
+
+    Args:
+        model (Union[str, Path]): Path to the ``Flair`` model to evaluate.
+        name (str): Name of the folder that will holds the resuts produced by
+            the ``Flair`` model.
+        evaluation_root (Union[str, Path]): Path to the root folder where the
+            results will be stored.
+        force (bool, optional): Force to create agin the golds standard files.
+            Defaults to False.
+    """
+    evaluation_root = Path(evaluation_root)
+
+    # Cf: Evaluation process on web.
+    golds_loc = evaluation_root / "golds"
+    sys_loc = evaluation_root / f"{name}"
+
+    if not Path(golds_loc).exists() or force:
+        gs_docs = GsDocs(ArchiveFolder.test)
+        gs_docs.to_gold_standard(golds_loc)
+
+    # TODO Is there a model name that can be extracted from the model to give
+    # automatically a name to the folder?
+
+    sys_docs = SysDocs(ArchiveFolder.test, model=model)
+    sys_docs.to_evaluation_folder(sys_loc)
