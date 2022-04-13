b0b7d46c96fcfb7c923bfd7c3ae59f35c14f4823
ggelabert <ggelabert@serikat.es>
2022-04-13 15:56:29 +0200

    :books: Update pigment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,6 +55,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
