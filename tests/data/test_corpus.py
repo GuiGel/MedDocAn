49eff8f22dd741060b97e0a510bf7ba964e35403
ggelabert <ggelabert@serikat.es>
2022-07-15 13:22:31 +0200

    :heavy_check_mark: Update doctest for test_corpus

--- tests/data/test_corpus.py
+++ tests/data/test_corpus.py
@@ -36,21 +36,9 @@ class TestMeddocan:
     An example of how we can test this class:
     Cf: https://docs.python.org/3/library/unittest.mock.html#patch
 
-        >>> from unittest.mock import patch
-        >>> class Class:
-        ...     def __iter__(self):
-        ...         for a in [1, 2, 3]:
-        ...             yield a
-        ...
-        >>> def function():
-        ...     return sum(a for a in Class())
-        ...
-        >>> with patch("__main__.Class") as MockClass:
-        ...     instance = MockClass.return_value
-        ...     instance.__iter__.return_value = [1, 2, 3]
-        ...     assert Class() is instance
-        ...     assert [a for a in Class()] == [1, 2, 3]
-        ...     assert function() == 6
+    In order to do that we patch the
+    :meth:``meddocan.data.docs_iterators.GsDocs.__iter__`` methods with the new
+    callable :class:``MockBratDocs``.
     """
 
     @pytest.mark.parametrize(
@@ -174,8 +162,8 @@ class TestMeddocan:
         is then internally copied to a temporary file by the custom
         :meth:`~meddocan.language.method_extensions.WriteMethods.__doc_to_connl03`
         of the Doc object. This is possible because the ``meddocan`` language
-        set the |Doc| extensions `to_connl03` and thus allow to use it on the
-        mocked documents.
+        set the |Doc| custom extensions `to_connl03` and thus allow to use it
+        on the mocked documents.
 
         Args:
             docs (Union[List[MockDoc], MockDoc]): The documents retrieve by
@@ -192,11 +180,11 @@ class TestMeddocan:
                 For Example:
 
                 >>> expected_contexts = (
-                ... ["Me", "llamo", Bertrand", "."],
-                ... "Vivo en Aix .",
-                ... ["Es", "una", "ciudad", "bonita", "."],
-                ... (2, 3, "LOC"),
-                ... ]
+                ...     ["Me", "llamo", "Bertrand", "."],
+                ...     "Vivo en Aix .",
+                ...     ["Es", "una", "ciudad", "bonita", "."],
+                ...     (2, 3, "LOC"),
+                ... )
         """
         with patch.object(
             GsDocs, "__iter__", new_callable=lambda: MockBratDocs(docs)
