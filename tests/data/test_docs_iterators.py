b46721a0686ac187acecd3c14d157787de0b7054
ggelabert <ggelabert@serikat.es>
2022-07-21 14:39:05 +0200

    :white_check_mark: Add minimal test for docs_iterators

--- tests/data/test_docs_iterators.py
+++ tests/data/test_docs_iterators.py
@@ -1,62 +1,197 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 import itertools as it
-from typing import List, Tuple
-from unittest.mock import MagicMock
+from typing import List
 
 import pytest
 from spacy.tokens import Doc
-from spacy.vocab import Vocab
 
-from meddocan.data.docs_iterators import GsDoc
+from meddocan.data import ArchiveFolder, meddocan_zip
+from meddocan.data.containers import BratSpan, ExpandedEntity
+from meddocan.data.docs_iterators import (
+    DocWithBratPair,
+    GsDoc,
+    get_expanded_entities,
+)
+from tests.language.test_method_extensions import MockDoc, TokenElements
+
+logger = logging.getLogger("meddocan")
 
 
-def create_docs(
-    texts: List[str],
-    sents_start: List[Tuple[int, ...]],
-    spaces: List[Tuple[int, ...]],
-) -> List[Doc]:
-    splitted_texts = [text.split("|") for text in texts]
-    vocab = Vocab(strings=list(it.chain(*splitted_texts)))
-    docs = [Doc(vocab, words) for words in splitted_texts]
-    for doc, sent_start, space in zip(docs, sents_start, spaces):
-        for i, token in enumerate(doc):
-            if i in sent_start:
-                token.sent_start = True
-            else:
-                token.sent_start = False
-            if i in space:
-                token.is_space = True
-    return docs
+class TestDocWithBratPair:
+    def test(self):
+        doc = (
+            MockDoc(
+                [
+                    TokenElements("Vivo", True, True),
+                    TokenElements("en", True, False),
+                    TokenElements("Aix", True, False, "B-LOC"),
+                    TokenElements("en", True, False, "I-LOC"),
+                    TokenElements("Provence", False, False, "I-LOC"),
+                    TokenElements("!", True, False),
+                    TokenElements("Soy", True, True),
+                    TokenElements("Eric", True, False, "B-PERS"),
+                    TokenElements("Laffont", False, False, "I-PERS"),
+                    TokenElements(".", True, False),
+                ],
+            ).get_spacy_doc(),
+        )
+        brat_file_pair = next(meddocan_zip.brat_files(ArchiveFolder.train))
+        doc_with_brat_pair = DocWithBratPair(brat_file_pair, doc)
+        assert str(doc_with_brat_pair) == (
+            "DocWithBratPair("
+            "brat_files_pair=BratFilesPair("
+            "ann=Path('/home/wave/.meddocan/datasets/meddocan/train-set.zip', "
+            "'train/brat/S0004-06142005000500011-1.ann'), "
+            "txt=Path('/home/wave/.meddocan/datasets/meddocan/train-set.zip', "
+            "'train/brat/S0004-06142005000500011-1.txt')), "
+            "doc=(Vivo en Aix en Provence! Soy Eric Laffont. ,))"
+        )
+
+
+@pytest.mark.parametrize(
+    argnames="doc",
+    argvalues=(
+        [
+            MockDoc(
+                [
+                    TokenElements("Vivo", True, True),
+                    TokenElements("en", True, False),
+                    TokenElements("Aix", True, False, "B-LOC"),
+                    TokenElements("en", True, False, "I-LOC"),
+                    TokenElements("Provence", False, False, "I-LOC"),
+                    TokenElements("!", True, False),
+                    TokenElements("Soy", True, True),
+                    TokenElements("Eric", True, False, "B-PERS"),
+                    TokenElements("Laffont", False, False, "I-PERS"),
+                    TokenElements(".", True, False),
+                ],
+            ).get_spacy_doc(),
+        ]
+    ),
+)
+@pytest.mark.parametrize(
+    argnames="brat_spans, expected",
+    argvalues=(
+        (
+            [
+                BratSpan(
+                    id=None,
+                    entity_type="LOC",
+                    start=2,
+                    end=5,
+                    text="Aix en Provence",
+                ),
+                BratSpan(
+                    id=None,
+                    entity_type="PERS",
+                    start=7,
+                    end=9,
+                    text="Eric Laffont",
+                ),
+            ],
+            [],
+        ),
+        (
+            [
+                BratSpan(
+                    id=None,
+                    entity_type="LOC",
+                    start=2,
+                    end=5,
+                    text="Aix en Provence",
+                ),
+                BratSpan(
+                    id=None,
+                    entity_type="PERS",
+                    start=7,
+                    end=8,
+                    text="EricLaffont",
+                ),
+            ],
+            [ExpandedEntity("EricLaffont", "Eric Laffont")],
+        ),
+    ),
+)
+def test_get_expanded_entities(
+    doc: Doc,
+    brat_spans: List[BratSpan],
+    expected: List[ExpandedEntity],
+) -> None:
+    """Test ``meddocan.data.docs_iterators.get_expanded_entities``.
+
+    Args:
+        doc (Doc): A spacy.tokens.Doc.
+        brat_spans (List[BratSpan]): The entities that the doc must contains.
+        expected (List[ExpandedEntity]): The expected results.
+    """
+    obtained = get_expanded_entities(doc, brat_spans)
+    for obtained_ee, expected_ee in it.zip_longest(obtained, expected):
+        assert obtained_ee == expected_ee
+
+
+@pytest.mark.parametrize(
+    argnames="doc",
+    argvalues=(
+        [
+            MockDoc(
+                [
+                    TokenElements("Vivo", True, True),
+                    TokenElements("en", True, False),
+                    TokenElements("Aix", True, False, "B-LOC"),
+                    TokenElements("en", True, False, "I-LOC"),
+                    TokenElements("Provence", False, False, "I-LOC"),
+                    TokenElements("!", True, False),
+                    TokenElements("Soy", True, True),
+                    TokenElements("Eric", True, False),
+                    TokenElements("Laffont", False, False),
+                    TokenElements(".", True, False),
+                ],
+            ).get_spacy_doc(),
+        ]
+    ),
+)
+@pytest.mark.parametrize(
+    argnames="brat_spans",
+    argvalues=(
+        [
+            BratSpan(
+                id=None,
+                entity_type="LOC",
+                start=2,
+                end=5,
+                text="Aix en Provence",
+            ),
+            BratSpan(
+                id=None,
+                entity_type="PERS",
+                start=7,
+                end=9,
+                text="Eric Laffont",
+            ),
+        ],
+    ),
+)
+def test_get_expanded_entities_bad_ents_num(
+    doc: Doc,
+    brat_spans: List[BratSpan],
+) -> None:
+    """Test that ``meddocan.data.docs_iterators.get_expanded_entities`` is
+    raising an :class:``AssertionError`` when the numbers of entities detected
+    are not the same a the expected ones.
+
+    Args:
+        doc (Doc): A spacy.tokens.Doc.
+        brat_spans (List[BratSpan]): The entities that the doc must contains.
+        expected (List[ExpandedEntity]): The expected results.
+    """
+    with pytest.raises(AssertionError):
+        get_expanded_entities(doc, brat_spans)
 
 
 class GsDoc:
     @pytest.mark.parametrize("text,expected_lines")
     def test_to_connl03(self, text, expected_lines):
         assert True
-
-
-if __name__ == "__main__":
-
-    sentences_1 = "Datos|del|paciente|.|\n|Remitido|por|:|Dra|.|Estefanía|Romero|Selas|.|Email|:|eromeroselas@yahoo|.|es"
-    sent_start_1 = [0, 5]
-    is_space_1 = [4]
-    sentences_2 = "Datos|del|paciente|.|\n|Remitido|por|:|Dr|:|Rodrigo|Martínez|Mansur|.|Email|:|drrmmmansur@yahoo|.|com"
-    sent_start_2 = [0, 5]
-    is_space_2 = [4]
-
-    docs = create_docs(
-        [sentences_1, sentences_2],
-        [sent_start_1, sent_start_2],
-        [is_space_1, is_space_2],
-    )
-    for doc in docs:
-        for sent in doc.sents:
-            print(f"{sent.text!r}")
-
-        # TODO Test the function create_docs
-
-        doc._.to_connl03 = MagicMock(return_value=1)
-        print(doc._.to_connl03("test.tsv", mode="w", write_sentences=True))
-
-    # Otherwise a more functional solution than a unitary one is to make a
-    # patch on __iter__ so that we return documents created by
-    # meddocan_pipeline
