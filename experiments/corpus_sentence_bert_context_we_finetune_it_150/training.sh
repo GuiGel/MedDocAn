73d8879af6cec5bb5f33bd32998477994f641f29
ggelabert <ggelabert@serikat.es>
2022-09-26 09:25:51 +0200

    :chart_with_upwards_trend: Update finetune + bert + context + we + it 150

--- experiments/corpus_sentence_bert_context_we_finetune_it_150/training.sh
+++ experiments/corpus_sentence_bert_context_we_finetune_it_150/training.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 set -x
 
-python experiments/corpus_sentence_bert_context_we_finetune_it_150/code_1.py
-python experiments/corpus_sentence_bert_context_we_finetune_it_150/code_12.py
+# python experiments/corpus_sentence_bert_context_we_finetune_it_150/code_1.py
+# python experiments/corpus_sentence_bert_context_we_finetune_it_150/code_12.py
 python experiments/corpus_sentence_bert_context_we_finetune_it_150/code_33.py
 python experiments/corpus_sentence_bert_context_we_finetune_it_150/get_metrics.py
