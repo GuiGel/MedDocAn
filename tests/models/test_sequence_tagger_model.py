704c39c8108a149713e7bcfd027bec006bb5c828
ggelabert <ggelabert@serikat.es>
2022-09-28 14:02:24 +0200

    :shirt: Update test

--- tests/models/test_sequence_tagger_model.py
+++ tests/models/test_sequence_tagger_model.py
@@ -1,8 +1,13 @@
-from meddocan.models import SequenceTagger
 from flair.data import Sentence
+
+from meddocan.models import SequenceTagger
+
 
 def test_predict():
     model = SequenceTagger.load("ner-fast")
     sentences = [Sentence("My name is Guillaume")]
     model.predict(sentences)
-    assert sentences.__str__() == '[Sentence: "My name is Guillaume" → ["Guillaume"/PER]]'
+    assert (
+        sentences.__str__()
+        == '[Sentence: "My name is Guillaume" → ["Guillaume"/PER]]'
+    )
