37a18729085cf74fdfed1bc0d0bda62f23ab2161
ggelabert <ggelabert@serikat.es>
2022-11-04 11:35:51 +0100

    :wrench: Update modify MYPYPATH

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-export MYPYPATH=./stubs
+export MYPYPATH=./stubs:./application
 mypy --show-error-code meddocan application
 black -l 79 meddocan tests application --check
 isort -l 79 meddocan tests application --check-only
