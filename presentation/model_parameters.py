c4c5f55e90f6779cb453367ea7dc3ff847b3514a
ggelabert <ggelabert@serikat.es>
2023-03-13 08:16:45 +0100

    :wrench: Add presentation to formatter % format

--- presentation/model_parameters.py
+++ presentation/model_parameters.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
-from flair.models.sequence_tagger_model import SequenceTagger
 import pandas as pd
 import torch
+from flair.models.sequence_tagger_model import SequenceTagger
+
 from meddocan.language.pipeline import MeddocanLanguage
+
 
 def get_parameter_num(model_path: Path) -> int:
     """Obtain the number of parameter in a model
@@ -18,38 +20,69 @@ def get_parameter_num(model_path: Path) -> int:
     parameters = model.parameters()
     return sum(map(torch.numel, parameters))
 
+
 def make_pretty(styler):
     styler.background_gradient(axis=None, cmap="YlGnBu")
-    styler.set_table_styles([
-        {'selector': 'th.col_heading.level0', 'props': 'text-align: center; font-size: 1.5em;'},
-        {"selector": "", "props": [("border", "1px solid grey")]},
-        {"selector": "tbody td", "props": [("border", "1px solid grey")]},
-        {"selector": "th", "props": [("border", "1px solid grey")]},
-    ], overwrite=False)
-    styler.format( "{:.0f}",)
+    styler.set_table_styles(
+        [
+            {
+                "selector": "th.col_heading.level0",
+                "props": "text-align: center; font-size: 1.5em;",
+            },
+            {"selector": "", "props": [("border", "1px solid grey")]},
+            {"selector": "tbody td", "props": [("border", "1px solid grey")]},
+            {"selector": "th", "props": [("border", "1px solid grey")]},
+        ],
+        overwrite=False,
+    )
+    styler.format(
+        "{:.0f}",
+    )
     return styler
+
 
 def get_models_parameters() -> pd.DataFrame:
     # The model are located on my public HuggingFace account
     mdpths = {
-        ("FINETUNE", "XLMR + WE"): "GuiGel/xlm-roberta-large-flert-we-finetune-meddocan",
-        ("FINETUNE", "XLMR"): "GuiGel/xlm-roberta-large-flert-finetune-meddocan",
-        ("FINETUNE", "BETO + WE"): "GuiGel/beto-uncased-flert-context-we-finetune-meddocan", 
+        (
+            "FINETUNE",
+            "XLMR + WE",
+        ): "GuiGel/xlm-roberta-large-flert-we-finetune-meddocan",
+        (
+            "FINETUNE",
+            "XLMR",
+        ): "GuiGel/xlm-roberta-large-flert-finetune-meddocan",
+        (
+            "FINETUNE",
+            "BETO + WE",
+        ): "GuiGel/beto-uncased-flert-context-we-finetune-meddocan",
         ("FINETUNE", "BETO"): "GuiGel/beto-uncased-flert-finetune-meddocan",
         ("LSTM CRF", "BETO"): "GuiGel/beto-uncased-flert-lstm-crf-meddocan",
-        ("LSTM CRF", "BETO + WE"): "GuiGel/beto-uncased-flert-context-we-lstm-crf-meddocan",
+        (
+            "LSTM CRF",
+            "BETO + WE",
+        ): "GuiGel/beto-uncased-flert-context-we-lstm-crf-meddocan",
         ("LSTM CRF", "FLAIR"): "GuiGel/meddocan-flair-lstm-crf",
         ("LSTM CRF", "FLAIR + WE"): "GuiGel/meddocan-flair-we-lstm-crf",
     }
 
     model_parameters_num = {k: get_parameter_num(v) for k, v in mdpths.items()}
 
-    df = (pd.DataFrame(model_parameters_num, index=["Numero de parámetros en Million"], dtype=int) / 1e6)
+    df = (
+        pd.DataFrame(
+            model_parameters_num,
+            index=["Numero de parámetros en Million"],
+            dtype=int,
+        )
+        / 1e6
+    )
 
     return df.style.pipe(make_pretty)
 
 
 def num_param(nlp: MeddocanLanguage) -> int:
     # The model are located on my public HuggingFace account
-    parameters = nlp._components[2][1].model.parameters()  # Bad code but it works...
+    parameters = nlp._components[2][
+        1
+    ].model.parameters()  # Bad code but it works...
     return sum(map(torch.numel, parameters))
--- presentation/utils.py
+++ presentation/utils.py
@@ -1,20 +1,23 @@
-from contextlib import contextmanager
-from typing import Iterable, Tuple, Iterator
 import base64
 import tempfile
-import requests
+from contextlib import contextmanager
 from pathlib import Path
+from typing import Iterable, Iterator, Tuple
+
+import requests
+from IPython.display import Markdown, display
 
 from meddocan.evaluation.classes import BratAnnotation
 
-from IPython.display import display, Markdown
 
 def display_script(loc: Path, language: str) -> None:
     script = Path(loc).read_text()
     display(Markdown(f"```{language}\n{script}\n```"))
 
+
 BASE = "https://api.github.com/repos/PlanTL-GOB-ES/MEDDOCAN-Evaluation-Script/contents"
 # The api where the text can be reach.
+
 
 def get_sample(base: str, name: str) -> str:
     # Get sample content from the folder located at https://github.com/PlanTL-GOB-ES/MEDDOCAN-Evaluation-Script/tree/master/gold/brat/sample via the Github api.
@@ -22,39 +25,49 @@ def get_sample(base: str, name: str) -> str:
     url = "/".join([base, name])
     req = requests.get(url)
     if req.status_code == requests.codes.ok:
-        req = req.json() # the response is JSON
+        req = req.json()  # the response is JSON
         # req is now a dict with keys: name, encoding, url, size ...
         # and content. But it is encoded with base64.
-        content = base64.b64decode(req['content'])
+        content = base64.b64decode(req["content"])
         return content.decode("utf-8")
     else:
         raise ValueError("Content was not found!")
 
+
 @contextmanager
-def write_text_to_tempdir(seq_file_text: Iterable[Tuple[str, str]]) -> Iterator[Path]:
+def write_text_to_tempdir(
+    seq_file_text: Iterable[Tuple[str, str]]
+) -> Iterator[Path]:
     # Context manager that write a sequence of (filename, text content) tuple
     # to a temporary directory and return the directory name.
     with tempfile.TemporaryDirectory() as tmpdirname:
         root = Path(tmpdirname)
         for loc, content in seq_file_text:
             (root / loc).write_text(content)
-        yield  Path(tmpdirname)
+        yield Path(tmpdirname)
 
-def get_brat_annotation_from_github(file: str, base: str = BASE) -> BratAnnotation:
+
+def get_brat_annotation_from_github(
+    file: str, base: str = BASE
+) -> BratAnnotation:
     ann = Path(file).with_suffix(".ann")
     txt = Path(file).with_suffix(".txt")
 
-    seq_file_text = ((loc.name, get_sample(base, str(loc))) for loc in [ann, txt])
+    seq_file_text = (
+        (loc.name, get_sample(base, str(loc))) for loc in [ann, txt]
+    )
 
     with write_text_to_tempdir(seq_file_text) as dir_loc:
         gold_annotation = BratAnnotation(str(dir_loc / Path(ann).name))
         return gold_annotation
+
 
 def glue_iob_label(iob: str, label: str) -> str:
     if len(label):
         return f"{iob}_{label}"
     else:
         return iob
+
 
 def get_parameter_num(model_path: Path) -> int:
     """Obtain the number of parameter in a model
@@ -69,13 +82,22 @@ def get_parameter_num(model_path: Path) -> int:
     parameters = model.parameters()
     return sum(map(torch.numel, parameters))
 
+
 def make_pretty(styler):
     styler.background_gradient(axis=None, cmap="YlGnBu")
-    styler.set_table_styles([
-        {'selector': 'th.col_heading.level0', 'props': 'text-align: center; font-size: 1.5em;'},
-        {"selector": "", "props": [("border", "1px solid grey")]},
-        {"selector": "tbody td", "props": [("border", "1px solid grey")]},
-        {"selector": "th", "props": [("border", "1px solid grey")]},
-    ], overwrite=False)
-    styler.format( "{:.0f}",)
+    styler.set_table_styles(
+        [
+            {
+                "selector": "th.col_heading.level0",
+                "props": "text-align: center; font-size: 1.5em;",
+            },
+            {"selector": "", "props": [("border", "1px solid grey")]},
+            {"selector": "tbody td", "props": [("border", "1px solid grey")]},
+            {"selector": "th", "props": [("border", "1px solid grey")]},
+        ],
+        overwrite=False,
+    )
+    styler.format(
+        "{:.0f}",
+    )
     return styler
